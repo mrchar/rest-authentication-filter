2c38e27a565264bfdd91f29378175b3262cb0eee
mrchar <mrchar@qq.com>
2023-03-03 13:56:18 +0800

    初始化项目

---
+++ src/main/java/net/mrchar/security/demo/RestAuthenticationFilterApplication.java
@@ -0,0 +1,13 @@
+package net.mrchar.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAuthenticationFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAuthenticationFilterApplication.class, args);
+    }
+
+}
